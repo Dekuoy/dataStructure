641d72e18cc9ac077f873fd45dc7841588bd55ae
dekuoy <2095839141@qq.com>
2025-02-25 15:46:09 +0300

    重构链表节点结构，修复内存分配和插入节点功能

--- lNode.c
+++ lNode.c
@@ -3,16 +3,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-/* typedef struct lNode{
-    int e;
-    struct lNode* next;
-}lNode , *LinkList; */
-
-typedef struct lNode{
+struct lNode{
     int e;
     struct lNode* next;
 };
-typedef struct lNode *LinkList;
+
+typedef struct lNode lNode ,*LinkList;
+
+/* typedef struct lNode{
+    int e;
+    struct lNode* next;
+}lNode , *LinkList;
+ */
+
 
 //无头结点的初始化
 void initLinkList1(LinkList l){
@@ -21,11 +24,33 @@ void initLinkList1(LinkList l){
 
 //有头结点的初始化
 bool initLinkList2(LinkList *l){
-    *l = (struct lNode*)malloc(sizeof(struct lNode));
+    *l = (lNode*)malloc(sizeof(lNode));
     if(*l ==NULL){
         return false;//内存不足,无法分配空间
     }
     (*l)->next = NULL;
+    return true;
+}
+
+
+//插入节点
+bool insertLink(LinkList l , int i ,int e){
+   lNode* p;//指针p指向当前扫描到的节点
+    p = l;
+    int j = 0;
+    while (p !=NULL && j < i-1)
+    {
+        p = p->next;
+        j++;
+    }
+    if(p == NULL){
+        return false;
+    }
+    lNode *s = (lNode*)malloc(sizeof(lNode*));
+    s->e = e;
+    s->next = p->next;
+    p->next = s;
+    
     return true;
 }
 
