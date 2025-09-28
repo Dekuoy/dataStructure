c25da19052d59305069f9fd91e0f6bc99824024b
Dekuoy <2095839141@qq.com>
2025-09-28 21:14:44 +0800

    同步

---
+++ LinkStack.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+struct LinkStackNode
+{
+    int data;
+    struct LinkStackNode *next;
+} ;
+typedef struct LinkStackNode* LinkStack;
+
+
+
---
+++ staticLinkList.c
@@ -0,0 +1,11 @@
+#define MaxSize 10
+struct SNode
+{
+    int data;
+    int next;
+};
+
+void testSLinkList(){
+    struct SNode a[MaxSize];
+}
+// 用数组实现的链表
